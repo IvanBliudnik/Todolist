7d6667f87324d6328ce414cb32ce431135dc8508
IvanBliudnik <Bliudnik007@gmail.com>
2024-05-19 19:17:07 +0300

    add idea to ignore

---
+++ src/Todolist.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {TaskType} from "./App";
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    date?:string
+}
+
+export const Todolist = ({ title, tasks }: PropsType) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            {/*...*/}
+            {tasks.map(task => {
+                debugger
+                return (
+                    <li key={task.id}>
+                        <input type="checkbox" checked={task.isDone} />
+                        <span>{task.title}</span>
+                    </li>
+                )
+            })}
+        </div>
+    )
+}
